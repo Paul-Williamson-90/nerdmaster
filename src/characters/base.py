e8a0e57adb45c04a21af5aa0f2d2e1cb5da50308
Paul-Williamson-90 <paulw.900430@gmail.com>
2024-03-27 08:54:17 +0000

    feat: Character base class

---
+++ src/characters/base.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from typing import List
+import numpy as np
+
+class Character(ABC):
+
+    def __init__(
+            self,
+            name: str,
+            inventory: List[str], # TODO: Replace with Item class
+            equipped_items: List[str], # TODO: Replace with Item/Equip class
+            gold: int,
+            health: str, # TODO: Replace with Health class
+            background: str, # TODO: Replace with Background class
+            memory: str, # TODO: Replace with Memory class
+            avatar: np.ndarray|None, # TODO: Replace with Avatar class
+            with_player: bool = False,
+            skills: List[str] = [], # TODO: Replace with Skill class
+    )->None:
+        self.name = name
+        self.inventory = inventory
+        self.equipped_items = equipped_items
+        self.gold = gold
+        self.health = health
+        self.background = background
+        self.memory = memory
+        self.avatar = avatar
+        self.with_player = with_player
+        self.skills = skills
+
+    @abstractmethod
+    def get_agent_tools(self):
+        pass
